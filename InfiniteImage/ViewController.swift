58debfa8bcd5f5203c19a146a16d144108c2588c
Mark DiFranco <mpdifran@gmail.com>
2017-04-29 12:04:24 -0400

    Initial Commit

---
+++ InfiniteImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfiniteImage
+//
+//  Created by Mark DiFranco on 2017-04-29.
+//  Copyright © 2017 MDFProjects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
